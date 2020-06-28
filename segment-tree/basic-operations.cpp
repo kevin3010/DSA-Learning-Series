2b673c25dd7a8b4c8941cae447a40dc7ab705a61
kevin3010 <42439376+kevin3010@users.noreply.github.com>
2020-06-28 18:03:00 +0530

    Update and rename segment-treee/basic-operations.cpp to segment-tree/basic-operations.cpp

--- segment-tree/basic-operations.cpp
+++ segment-tree/basic-operations.cpp
@@ -1,3 +1,5 @@
+// range querry for minimum in an array
+
 void bulidTree(int idx,int left,int right)
 {
 	if(left==right)
@@ -22,4 +24,20 @@ int queryTree(int idx ,int ss,int se,int qs,int qe)
 	int r=queryTree(idx*2+1, mid+1,se, max(mid+1,qs), qe);
 
 	return min(l,r);
+}
+
+void update(int idx, int ss,int se, int qi)
+{
+	if(ss==se){
+		seg[idx] = arr[qi];
+		return;
+	}
+		
+	int mid = (ss+se)/2;
+	if(qi<mid+1)
+		update(idx*2,ss,mid,qi);
+	else
+		update(idx*2+1, mid+1,se, qi);
+
+	seg[idx] = min(seg[idx*2] , seg[idx*2+1]);
 }
